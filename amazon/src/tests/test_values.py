0aa8c05f3fd93bdaa43bdf01fdc72e1f3d74bb60
Marwan Sultan <marwan.sultan@gmail.com>
2021-04-28 19:15:59 -0400

    data driven test

--- amazon/src/tests/test_values.py
+++ amazon/src/tests/test_values.py
@@ -6,43 +6,40 @@ import json
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
 from selenium.webdriver.common.keys import Keys
-import attr
 
 
+def validate_item_count(self, driver):
+    self.driver = driver
+    product_count = driver.find_element_by_css_selector(
+        "#search > span > div > span > h1 > div > div.sg-col-14-of-20.sg-col.s-breadcrumb.sg-col-10-of-16.sg-col-6-of-12 > div > div > span:nth-child(1)")
+    assert(product_count)
 
-        
+
+def validate_clickable_item(self, driver):
+    self.driver = driver
+    clickable_item = driver.find_element_by_css_selector(
+        "#search > span > div > span > h1 > div > div.sg-col-14-of-20.sg-col.s-breadcrumb.sg-col-10-of-16.sg-col-6-of-12 > div > div > span:nth-child(1)")
+    assert(clickable_item)
+
+
 def test_loop():
-    
-    
-        
-        
+
     items = {"Samsung Widescreen Monitor", "Vizio", "Alexa",
-        "hp", "Dell Computers", "Advanced projectors", "Dewalt Power Tools"}
-            
-            
-            
+             "hp", "Dell Computers", "Advanced projectors", "Dewalt Power Tools"}
+
     for i in items:
-                
-                #Install ChromeDriverManager, open a browser and navigate to Amazon.com
-                print('initializing ChromeDriver')
-                driver = webdriver.Chrome(ChromeDriverManager().install())
-                #Navigate to the URL under test.
-                driver.get('https://www.amazon.com')
-                # Maximize the browser.
-                driver.maximize_window()
-                # Enter a product in the search field.
-                search = driver.find_element_by_id('twotabsearchtextbox')
-                search.send_keys(i)
-                search.send_keys(Keys.ENTER)
-                product_count = driver.find_element_by_css_selector(
-                    '#search > span > div > span > h1 > div > div.sg-col-14-of-20.sg-col.s-breadcrumb.sg-col-10-of-16.sg-col-6-of-12 > div > div > span:nth-child(1)')
-                clickable_item = driver.find_element_by_css_selector(
-                    "#search > span > div > span > h1 > div > div.sg-col-14-of-20.sg-col.s-breadcrumb.sg-col-10-of-16.sg-col-6-of-12 > div > div > span:nth-child(1)")
-                assert(product_count)
-                assert(clickable_item)
-                driver.close()
 
+        #Install ChromeDriverManager, open a browser and navigate to Amazon.com
+        print('initializing ChromeDriver')
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        #Navigate to the URL under test.
+        driver.get('https://www.amazon.com')
+        # Maximize the browser.
+        driver.maximize_window()
+        # Enter a product in the search field.
+        search = driver.find_element_by_id('twotabsearchtextbox')
+        search.send_keys(i)
+        search.send_keys(Keys.ENTER)
 
-
+        driver.close()
